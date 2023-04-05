ccdc0eaf5825ceaff88f46bc53119b2033b90191
Ginicoefficient <104734969+Ginicoefficient@users.noreply.github.com>
2023-04-05 17:48:57 -0500

    so close to learning state

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import memeData from "../Data.js";
 
 export default function Meme() {
+  function getMemeHandler() {
+    console.log(getRandomImage());
+  }
+
+  function getRandomImage() {
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    console.log(randomNumber);
+    const url = memesArray[randomNumber].url;
+    return url;
+  }
+
   return (
     <div className="<Meme--container">
-      <form className="form">
+      <div className="form">
         <input type="text" className="form--input" placeholder="Top Text" />
         <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+        <button className="form--button" onClick={getMemeHandler}>
+          Get a new meme image 🖼{" "}
+        </button>
+      </div>
     </div>
   );
 }
