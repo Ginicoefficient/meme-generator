0175cdf163d69d5430e5533f5bf8273336ea21e0
Virginia Mitchell <ginny.e.mitchell@gmail.com>
2023-04-06 15:13:22 -0500

    using state changes to update meme url

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import memeData from "../Data.js";
 
 export default function Meme() {
+  const [image, setImage] = React.useState("");
+
   function getMemeHandler() {
-    console.log(getRandomImage());
+    const currentMeme = getRandomImage();
+    setImage(currentMeme);
   }
 
   function getRandomImage() {
     const memesArray = memeData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    console.log(randomNumber);
     const url = memesArray[randomNumber].url;
     return url;
   }
 
   return (
-    <div className="<Meme--container">
+    <div className="meme--container">
       <div className="form">
         <input type="text" className="form--input" placeholder="Top Text" />
         <input type="text" className="form--input" placeholder="Bottom Text" />
         <button className="form--button" onClick={getMemeHandler}>
           Get a new meme image 🖼{" "}
         </button>
+      </div>
+      <div className="meme--img-container">
+        {image && <img className="meme--img" src={image} />}
       </div>
     </div>
   );
