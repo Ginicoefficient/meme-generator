b651c2d6d9f213f2744ea8c75a118cddf9819c9c
Virginia Mitchell <ginny.e.mitchell@gmail.com>
2023-04-19 11:55:32 -0500

    like  a few more things

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -8,6 +8,15 @@ export default function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
   const [allMemeImages, setAllMemeImages] = React.useState(memeData);
 
   function getMemeHandler() {
@@ -29,16 +38,34 @@ export default function Meme() {
   return (
     <div className="meme--container">
       <div className="form">
-        <input type="text" className="form--input" placeholder="Top Text" />
-        <input type="text" className="form--input" placeholder="Bottom Text" />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top Text"
+          name="topText"
+          onChange={handleChange}
+          value={meme.topText}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom Text"
+          name="bottomText"
+          onChange={handleChange}
+          value={meme.bottomText}
+        />
         <button className="form--button" onClick={getMemeHandler}>
           Get a new meme image 🖼{" "}
         </button>
       </div>
-      <div className="meme--img-container">
-        {meme.randomImage && (
-          <img className="meme--img" src={meme.randomImage} />
-        )}
+      <div className="meme--constructor">
+        <div className="meme--img-container">
+          {meme.randomImage && (
+            <img className="meme--img" src={meme.randomImage} />
+          )}
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
       </div>
     </div>
   );
